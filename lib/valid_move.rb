e41009e4d672c99084d39e64b683ccc5e4fb75ba
Ted Barlow <ted@noukod.com>
2018-09-02 13:52:30 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,5 +1,14 @@
 # code your #valid_move? method here
-def valid_move?(index)
+def valid_move?(board, index)
+  if position_taken?(board, index)
+    return position_taken?
+  end
+    if index.between?(0, 8)
+      return true
+    else
+      return false
+    end
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, index)
